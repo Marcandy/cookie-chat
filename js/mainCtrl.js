50172916ac42ed8b559dbae4c9b392fa8a368b06
marcandy <marcandy.nj@gmail.com>
2016-12-14 18:06:29 -0700

    review

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -39,12 +39,13 @@ angular.module('chatroom').controller('mainCtrl', function($scope, messageServic
     })
     $scope.cookie = '';
   }
-  //uncomment this code when your getMessages function is finished
-  //This goes and gets new data every second, which mimicking a chat room experience.
-  //  setInterval(function(){
-  //   $scope.getMessages();
-  //
-  // }, 10500)
+
+  // uncomment this code when your getMessages function is finished
+  // This goes and gets new data every second, which mimicking a chat room experience.
+   setInterval(function(){
+    $scope.getMessages();
+
+  }, 10500)
 
 
 
